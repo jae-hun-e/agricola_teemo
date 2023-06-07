124b678c1c366187005ebd5b0049a95d1269900b
jaeyoung0110 <whwodud231@gmail.com>
2023-06-08 05:34:31 +0900

    asset 수정

--- src/components/Box/ActionBox.tsx
+++ src/components/Box/ActionBox.tsx
@@ -1,11 +1,11 @@
-import { cls } from "@utils/util";
-import { IBaseCards } from "@ITypes/play";
+import {cls} from "@utils/util";
+import {IBaseCards} from "@ITypes/play";
 import ModalButton from "@components/Button/ModalButton";
-import { sendActionSocket, sendAdditionalSocket } from "@utils/socket";
+import {sendActionSocket, sendAdditionalSocket} from "@utils/socket";
 import JobCard from "@components/Card/JobCard";
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { gamePlayData } from "@atom/gamePlayData";
+import {useState} from "react";
+import {useRecoilValue} from "recoil";
+import {gamePlayData} from "@atom/gamePlayData";
 
 interface Props {
   client: WebSocket | null;
@@ -13,9 +13,9 @@ interface Props {
   layout?: string;
   imgidx?: string;
 }
-const ActionBox = ({ client, layout, base_cards, imgidx }: Props) => {
+const ActionBox = ({client, layout, base_cards, imgidx}: Props) => {
   const [additionallCard, setAdditionalCard] = useState<string>("");
-  const { players } = useRecoilValue(gamePlayData);
+  const {players} = useRecoilValue(gamePlayData);
   const myJobCard = players[0].cards.slice(0, 7);
   const mySubFacilityCard = players[0].cards.slice(7);
 
@@ -25,8 +25,7 @@ const ActionBox = ({ client, layout, base_cards, imgidx }: Props) => {
   const handleAction = () => {
     base_cards.player !== null
       ? alert("다른 player가 있는 칸은 선택할 수 없습니다.")
-      : base_cards.card_number === "BASE_05" ||
-        base_cards.card_number === "BASE_08"
+      : base_cards.card_number === "BASE_05" || base_cards.card_number === "BASE_08"
       ? sendAdditionalSocket(client, base_cards, 0, additionallCard)
       : sendActionSocket(client, base_cards, 0);
     console.log("user action_", imgidx);
@@ -36,64 +35,62 @@ const ActionBox = ({ client, layout, base_cards, imgidx }: Props) => {
       <img
         src={`/images/mainboard/${imgidx}.png`}
         alt=""
-        className={cls(
-          "w-[100px] rounded-md cursor-pointer",
-          layout ? layout : "h-[70px]"
-        )}
+        className={cls("w-[100px] rounded-md cursor-pointer", layout ? layout : "h-[70px]")}
       />
       {base_cards.player !== null && (
         <div
           className="absolute w-[40px] h-[40px] bg-contain bg-center bg-no-repeat top-[10px] left-[30px]"
           style={{
-            backgroundImage: `url('/images/mainboard/item${
-              13 + base_cards.player
-            }.png')`,
+            backgroundImage: `url('/images/mainboard/item${13 + base_cards.player}.png')`,
           }}
         />
       )}
       <ModalButton
-        layoutCSS={cls(
-          "w-[100px] cursor-pointer absolute top-0",
-          layout ? layout : "h-[70px]"
-        )}
-        childrenCSS="w-[800px] h-[600px]"
+        layoutCSS={cls("w-[100px] cursor-pointer absolute top-0", layout ? layout : "h-[70px]")}
+        childrenCSS="w-[700px] h-[600px]"
         type="action"
         handleAction={handleAction}
       >
         {base_cards.card_number === "BASE_05" ? (
           //   교습
-          <div className="relative flex justify-center">
-            <div className="flex gap-[10px] mt-[50px]">
-              {myJobCard.map((data, i) => (
-                <div
-                  key={i}
-                  className={cls(
-                    "w-[100px] h-[200px] bg-center bg-no-repeat",
-                    "border-solid border-red-500",
-                    data.is_use ? "border-[5px]" : ""
-                  )}
-                  style={{
-                    backgroundImage: `url('/assets/${data.card_number}.png')`,
-                  }}
-                  onClick={() => onClick(data)}
-                ></div>
-              ))}
+          <div className="flex flex-col justify-center items-center">
+            <p className="mt-[10px]">교습 : 직업카드</p>
+            <div className="relative flex justify-center">
+              <div className="grid grid-cols-4 gap-[10px] mt-[20px]">
+                {myJobCard.map((data, i) => (
+                  <div
+                    key={i}
+                    className={cls(
+                      "w-[136px] h-[212px] bg-cover rounded-md bg-center bg-no-repeat",
+                      "border-solid border-red-500",
+                      data.is_use ? "border-[5px]" : ""
+                    )}
+                    style={{
+                      backgroundImage: `url('/assets/${data.card_number}.png')`,
+                    }}
+                    onClick={() => onClick(data)}
+                  ></div>
+                ))}
+              </div>
             </div>
           </div>
         ) : base_cards.card_number === "BASE_08" ? (
           //   화합장소
-          <div className="relative flex justify-center">
-            <div className="flex gap-[10px] mt-[50px]">
-              {mySubFacilityCard.map((data, i) => (
-                <div
-                  key={i}
-                  className="w-[100px] h-[200px] bg-center bg-no-repeat"
-                  style={{
-                    backgroundImage: `url('/assets/${data.card_number}.png')`,
-                  }}
-                  onClick={() => onClick(data)}
-                ></div>
-              ))}
+          <div className="flex flex-col justify-center items-center">
+            <p className="mt-[10px]">회합 장소 : 보조 설비</p>
+            <div className="relative flex justify-center">
+              <div className="grid grid-cols-4 gap-[10px] mt-[20px]">
+                {mySubFacilityCard.map((data, i) => (
+                  <div
+                    key={i}
+                    className="w-[136px] h-[212px] rounded-md bg-cover bg-center bg-no-repeat"
+                    style={{
+                      backgroundImage: `url('/assets/${data.card_number}.png')`,
+                    }}
+                    onClick={() => onClick(data)}
+                  ></div>
+                ))}
+              </div>
             </div>
           </div>
         ) : (
