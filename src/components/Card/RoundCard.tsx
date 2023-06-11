7d5a63c13f277ffb38222e6f4b7edecb2c0fecbe
MinJunsu <sunsuking@gmail.com>
2023-06-12 03:53:17 +0900

    라운드 카드에 추가 획득 자원 추가

--- src/components/Card/RoundCard.tsx
+++ src/components/Card/RoundCard.tsx
@@ -3,6 +3,9 @@ import { IRoundCards } from "@ITypes/play";
 import AdditionalModalButton from "@components/Button/AdditionallModalButton";
 import ActionModalButton from "@components/Button/ActionModalButton";
 import { roundAdditionalCardOpen } from "@constants/cardCase";
+import { gamePlayData } from "@atom/gamePlayData";
+import { useRecoilValue } from "recoil";
+import Image from "next/image";
 
 interface Props {
   client: WebSocket | null;
@@ -10,19 +13,87 @@ interface Props {
   round_cards: IRoundCards;
   idx: number;
 }
-const RoundCard = ({ client, layoutCSS, round_cards }: Props) => {
+
+const RoundCard = ({ client, layoutCSS, round_cards, idx }: Props) => {
+  const { round } = useRecoilValue(gamePlayData);
+    const leftTop: any = Object.values(round_cards.additional_action)[0];
+    const leftBottom: any = Object.values(round_cards.additional_action)[1];
+    const rightTop: any = Object.values(round_cards.additional_action)[2];
+    const rightBottom: any = Object.values(round_cards.additional_action)[3];
+    console.log(leftTop);
   // 비활성화
-  // if (idx > round + 1)
-  //   return (
-  //     <div
-  //       className="w-[100px] h-[150px] bg-cover rounded-md relative"
-  //       style={{
-  //         backgroundImage: `url('/assets/ACTION_${
-  //           idx < 10 ? `0${idx}` : idx
-  //         }_FLIPPED.png')`,
-  //       }}
-  //     />
-  //   );
+  if (idx > round + 1)
+    return (
+      <div
+        className="w-[100px] h-[150px] bg-cover rounded-md relative"
+        style={{
+          backgroundImage: `url('/assets/ACTION_${
+            idx < 10 ? `0${idx}` : idx
+          }_FLIPPED.png')`,
+        }}
+      >
+          {
+              Object.entries(leftTop).map((item, idx) => {
+                  if (item[1] !== 0) {
+                      return <div key={idx} className="absolute -left-1" style={{
+                          top: idx * 2,
+                          zIndex: 20 + idx,
+                      }}>
+                          <div className="flex flex-row gap-0.5">
+                              <Image src={`/images/mainboard/item${idx + 1}.png`} alt={"leftTop"} width={20} height={20} />
+                              <span className="text-white text-xs">X{item[1]}</span>
+                          </div>
+                      </div>;
+                  }
+              })
+          }
+          {
+              Object.entries(leftBottom).map((item, idx) => {
+                  if (item[1] !== 0) {
+                      return <div key={idx} className="absolute -left-1" style={{
+                          bottom: idx * 2,
+                          zIndex: 20 + idx,
+                      }}>
+                          <div className="flex flex-row gap-0.5">
+                              <Image src={`/images/mainboard/item${idx + 1}.png`} alt={"leftTop"} width={20} height={20} />
+                              <span className="text-white text-xs">X{item[1]}</span>
+                          </div>
+                      </div>;
+                  }
+              })
+          }
+          {
+              Object.entries(rightTop).map((item, idx) => {
+                  if (item[1] !== 0) {
+                      return <div key={idx} className="absolute -right-1" style={{
+                          top: idx * 2,
+                          zIndex: 20 + idx,
+                      }}>
+                          <div className="flex flex-row gap-0.5">
+                              <Image src={`/images/mainboard/item${idx + 1}.png`} alt={"leftTop"} width={20} height={20} />
+                              <span className="text-white text-xs">X{item[1]}</span>
+                          </div>
+                      </div>;
+                  }
+              })
+          }
+          {
+              Object.entries(rightBottom).map((item, idx) => {
+                  if (item[1] !== 0) {
+                      return <div key={idx} className="absolute -right-1" style={{
+                          bottom: idx * 2,
+                          zIndex: 20 + idx,
+                      }}>
+                          <div className="flex flex-row gap-0.5">
+                              <Image src={`/images/mainboard/item${idx + 1}.png`} alt={"leftTop"} width={20} height={20} />
+                              <span className="text-white text-xs">X{item[1]}</span>
+                          </div>
+                      </div>;
+                  }
+              })
+          }
+      </div>
+    );
 
   return (
     <div
@@ -36,7 +107,7 @@ const RoundCard = ({ client, layoutCSS, round_cards }: Props) => {
     >
       {round_cards.player !== null && (
         <div
-          className="absolute w-[40px] h-[40px] bg-contain bg-center bg-no-repeat left-[30px] bottom-[10px]"
+          className="absolute w-[40px] h-[40px] bg-contain bg-center bg-no-repeat left-[30px] bottom-[10px] z-10"
           style={{
             backgroundImage: `url('/images/mainboard/item${
               13 + round_cards.player
