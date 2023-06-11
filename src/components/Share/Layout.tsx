a3a95f5b378df204dbd486c3bf582a32f0910fa1
hun2__2 <wognskec@hanyang.ac.kr>
2023-06-12 05:06:29 +0900

    bg 수정

--- src/components/Share/Layout.tsx
+++ src/components/Share/Layout.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import Header from "@components/Share/Header";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex justify-center bg-[#a3cd31]">
-      <div className="w-fScreen flex flex-col justify-center ">
+    <div className="flex justify-center relative">
+      <div className="fixed bg-[#a3cd31] w-full h-[100vh] -z-10" />
+      <div className="w-fScreen flex flex-col justify-center">
         <Header />
         <div className="w-full h-[100vh] ">{children}</div>
       </div>
